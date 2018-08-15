4bcb6918b040a9192091a119fd35904a671fb87b
Josh Cesana <joshuacesana@gmail.com>
2018-08-14 19:08:57 -0700

    add migration, refactor models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :lists
-
+  has_secure_password
   validates :username, presence: true
   validates :email, presence: true
   validates :password, presence: true
