73318b2f08a7049128191b09d40244fe018dfb13
Josh Cesana <joshuacesana@gmail.com>
2018-08-15 10:38:45 -0700

    add slug, self.find_by_slug methods to list model

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,4 +2,14 @@ class List < ActiveRecord::Base
   belongs_to :user
   has_many :tasks
   validates :name, presence: true
+
+  def slug
+    self.name.gsub(' ', '-').downcase
+  end
+
+  def self.find_by_slug(slug)
+    self.all.detect do |list|
+      user.slug == slug
+    end
+  end
 end
