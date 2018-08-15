e2cb0ea519659b3395de4f03762bc12634f517d5
Josh Cesana <joshuacesana@gmail.com>
2018-08-14 21:20:34 -0700

    add helpers to application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,23 @@ class ApplicationController < Sinatra::Base
   get '/' do
     erb :index
   end
+
+  helpers do
+    def redirect_if_not_logged_in
+      if !logged_in?
+          redirect “/failure”
+      end
+    end
+
+    def logged_in?(session)
+      !!session[:user_id]
+    end
+
+    def current_user(session)
+      User.find_by_id(session[:user_id])
+    end
+
+    def self.slug(user)
+    end
+  end
 end
