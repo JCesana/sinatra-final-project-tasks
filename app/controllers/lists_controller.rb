cd5bb2608ccb222363981fb2f0abfef100faaf6e
Josh Cesana <joshuacesana@gmail.com>
2018-08-14 22:43:11 -0700

    refactor lists controller, add lists/new view

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -7,4 +7,15 @@ class ListsController < ApplicationController
     erb :lists
   end
 
+  get '/lists/new' do
+    redirect_if_not_logged_in
+    @user = current_user(session)
+
+    erb :"/lists/new"
+  end
+
+  post '/lists' do
+    binding.pry 
+  end
+
 end
