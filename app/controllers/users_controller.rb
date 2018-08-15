9c918d2914e6bad828a0b0215dd917eaaf5138da
Josh Cesana <joshuacesana@gmail.com>
2018-08-15 12:05:21 -0700

    refactor useres controller and show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,8 @@
 class UsersController < ApplicationController
 
   get '/users/:slug' do
-    binding.pry
     @user = User.find_by_slug(params[:slug].downcase)
-    binding.pry
+    
     erb :'/users/show'
   end
 
