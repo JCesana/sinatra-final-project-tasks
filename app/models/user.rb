b37cc3aec0bb1b659459ee50150a1cf144781dbb
Josh Cesana <joshuacesana@gmail.com>
2018-08-14 18:20:28 -0700

    Add models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
+  has_many :lists
 
+  validates :username, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
 end
--- db/migrate/20180815010514_create_tasks.rb
+++ db/migrate/20180815010514_create_tasks.rb
@@ -2,7 +2,6 @@ class CreateTasks < ActiveRecord::Migration
   def change
     create_table :tasks do |t|
       t.string :name
-      t.integer :list_id 
     end
   end
 end
