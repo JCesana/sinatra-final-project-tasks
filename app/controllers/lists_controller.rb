176fa298285601c249de44db28faa1fb498b673e
Josh Cesana <joshuacesana@gmail.com>
2018-08-15 12:43:23 -0700

    refactor list controller, edit and show views

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -26,10 +26,37 @@ class ListsController < ApplicationController
     end
   end
 
+  get '/lists/:slug/edit' do
+    redirect_if_not_logged_in
+    @user = current_user(session)
+
+    @list = List.find_by_slug(params[:slug])
+
+    erb :"/lists/edit"
+  end
+
   get '/lists/:slug' do
     @list = List.find_by_slug(params[:slug])
 
     erb :"/lists/show"
+  end
+
+  patch "/lists/:slug" do
+    redirect_if_not_logged_in
+    @user = current_user(session)
+
+    valid_list = List.new(params[:list])
+
+    if valid_list.valid?
+      @list = List.find_by_slug(params[:slug])
+      @list.update(params[:list])
+      @list.save
+
+      redirect "/lists/#{@list.slug}"
+    else
+      redirect "/lists/#{@list.slug}/edit"
+    end
+
   end
 
 end
