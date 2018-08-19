ebc249f1952e906fadbf1b5d83e860c74b544a32
Josh Cesana <joshuacesana@gmail.com>
2018-08-18 21:37:56 -0700

    updated db seeds for testing

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 users = [
-  {username: 'johnny121', email: 'johnny@yahoo.com', password: "123" },
-  {username: 'kate2', email: 'kate22@aol.com', password: "123" },
-  {username: 'lizzyC', email: 'LizAndCam@gmail.com', password: "123" },
-  {username: 'Joshc3', email: 'josh@gmail.com', password: "123" }
+  {username: 'JohnJax', email: 'johnny@yahoo.com', password: "123" },
+  {username: 'HollyHunt', email: 'kate22@aol.com', password: "123" },
+  {username: 'OliverTwist', email: 'LizAndCam@gmail.com', password: "123" },
+  {username: 'VinceVee', email: 'josh@gmail.com', password: "123" }
 ]
 
 users.each do |u|
@@ -24,7 +24,7 @@ tasks = [
   {name: "Run a marathon", list_id: 1}, {name: "Find soulmate", list_id: 1}, {name: "Win the lottery", list_id: 1},
   {name: "Bailey", list_id: 2}, {name: "Max", list_id: 2}, {name: "Charlie", list_id: 2}, {name: "Donald", list_id: 2},
   {name: "Dusting", list_id: 3}, {name: "Vacuuming", list_id: 3}, {name: "Sweeping", list_id: 3}, {name: "Feed goldfish", list_id: 3},
-  {name: "Check your facts", list_id: 4}, {name: "He's a great guy...", list_id: 4}, {name: "Billions and billions of dollars...", list_id: 4}, {name: "Winning again", list_id: 4},
+  {name: "Check your facts", list_id: 4}, {name: "He's a great guy...", list_id: 4}, {name: "Billions and billions of dollars...", list_id: 4}, {name: "Bing, bing bong!", list_id: 4},
 ]
 
 tasks.each do |t|
